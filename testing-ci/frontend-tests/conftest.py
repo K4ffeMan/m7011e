0c6972065eab10e34f97678d3fcda57c5a520fa5
Kebert <rasmus.kebert@gmail.com>
2025-12-23 14:29:31 +0100

    Changed a test and some things in conftest to work with github

--- testing-ci/frontend-tests/conftest.py
+++ testing-ci/frontend-tests/conftest.py
@@ -1,18 +1,18 @@
+import os
 import pytest
 from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
 def browser():
+    headless = os.getenv("CI") == "true"
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=headless)
         yield browser
         browser.close()
 
 @pytest.fixture
 def context(browser):
-    context = browser.new_context(
-        ignore_https_errors=True
-    )
+    context = browser.new_context(ignore_https_errors=True)
     yield context
     context.close()
 
