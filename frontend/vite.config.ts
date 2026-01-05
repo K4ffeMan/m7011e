52dcb9c5d4517f4c2fec9fe1ec14c0437c4c2e3b
Kebert <rasmus.kebert@gmail.com>
2026-01-05 13:45:48 +0100

    might work for argo now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,23 +4,4 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: [
-      "frontend-dev.ltu-m7011e-7.se"
-    ],
-    proxy: {
-      "/api": {
-        target: "http://backend:5000", // your backend URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  preview: {
-    host: true,
-    allowedHosts: [
-      "frontend-dev.ltu-m7011e-7.se",
-    ],
-  },
 });
