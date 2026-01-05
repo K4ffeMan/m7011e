d724121d92da8db580f485c26ebcbd8e5bb85c4c
Kebert <rasmus.kebert@gmail.com>
2026-01-05 13:01:31 +0100

    Added so frontend-dev can use vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: [
+      "https://frontend-dev.ltu-m7011e-7.se"
+    ],
     proxy: {
       "/api": {
         target: "http://localhost:5000", // your backend URL
